54bac2a1d0c2c1f14ec31a044134cf86396d4402
LiudmilaLV <lisikhina.lv@gmail.com>
2023-03-28 15:50:20 +0700

    implement displaying of a square grid

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-    function createDiv() {
-        let boardDiv = document.createElement("div");
-        boardDiv.className = "new-div";
-        boardDiv.innerText = "I am a new DIV";
-        return boardDiv;
-    }
-
-    function createAndModifyDivs() {
-        let board = document.getElementById("board");
-        let myDivs = [];
-        let numOfDivs = 9;
-
-        for (let i = 0; i < numOfDivs; i += 1) {
-        myDivs.push(createDiv());
-        board.appendChild(myDivs[i]);
+    const button = document.getElementById("get-num-button");
+    const numField = document.getElementById("num");
+    
+    button.addEventListener("click", () => {
+        
+        function createDiv() {
+            let boardDiv = document.createElement("div");
+            boardDiv.className = "new-div";
+            boardDiv.style.minWidht = "2rem";
+            return boardDiv;
         }
-    // set grid styles to the board
-    }
 
-    createAndModifyDivs();
+        function modifyGrid() {
+            let numOfSquares = parseInt(numField.value, 10)
+            let board = document.getElementById("board");
+            let myDivs = [];
+            let numOfDivs = numOfSquares * numOfSquares;
+
+            // set grid styles to the board
+            board.style.gridTemplateColumns = `repeat(${numOfSquares}, 1fr)`;
+            board.style.gridTemplateRows = `repeat(${numOfSquares}, 1fr)`;
+
+            for (let i = 0; i < numOfDivs; i += 1) {
+            myDivs.push(createDiv());
+            }
+            board.replaceChildren(...myDivs);
+        }
+    modifyGrid();
+    });
+
 });
