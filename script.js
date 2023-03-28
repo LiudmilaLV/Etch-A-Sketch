e40145148759c26e780a9f46586d47d53ecdfcd0
LiudmilaLV <lisikhina.lv@gmail.com>
2023-03-28 13:42:45 +0700

    switch to css grid layout

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-// width of individual div in the grid = width of the page / number of columns
-// that way it woud wrap into a square grid
+document.addEventListener("DOMContentLoaded", () => {
+    function createDiv() {
+        let boardDiv = document.createElement("div");
+        boardDiv.className = "new-div";
+        boardDiv.innerText = "I am a new DIV";
+        return boardDiv;
+    }
+
+    function createAndModifyDivs() {
+        let board = document.getElementById("board");
+        let myDivs = [];
+        let numOfDivs = 9;
+
+        for (let i = 0; i < numOfDivs; i += 1) {
+        myDivs.push(createDiv());
+        board.appendChild(myDivs[i]);
+        }
+    // set grid styles to the board
+    }
+
+    createAndModifyDivs();
+});
